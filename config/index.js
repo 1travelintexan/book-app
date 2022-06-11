4e72d9d310326cd2a976e01d90c418ca80adb4b1
Joshua George <horheyinc8@gmail.com>
2022-06-11 18:55:57 +0200

    favicon deleted

--- config/index.js
+++ config/index.js
@@ -9,10 +9,6 @@ const logger = require("morgan");
 // https://www.npmjs.com/package/cookie-parser
 const cookieParser = require("cookie-parser");
 
-// ℹ️ Serves a custom favicon on each request
-// https://www.npmjs.com/package/serve-favicon
-// const favicon = require("serve-favicon");
-
 // ℹ️ global package used to `normalize` paths amongst different operating systems
 // https://www.npmjs.com/package/path
 const path = require("path");
@@ -33,7 +29,4 @@ module.exports = (app) => {
   app.set("view engine", "hbs");
   // Handles access to the public folder
   app.use(express.static(path.join(__dirname, "..", "public")));
-
-  // Handles access to the favicon
-  // app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
 };
